75838c1bd383d52bd1da1fe7da52c1452c3746a1
parallelyk <lucky.seven@qq.com>
2016-06-11 17:17:14 +0800

    update

---
+++ app/src/main/java/com/parallelyk/picturedisplaydemo/MyImageView.java
@@ -0,0 +1,14 @@
+package com.parallelyk.picturedisplaydemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by YK on 2016/6/11.
+ */
+public class MyImageView extends View {
+    public MyImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
